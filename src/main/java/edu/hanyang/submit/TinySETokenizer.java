43008fd507730a310b94b1a9407418ed9f1c3ace
박민응 <nadasansu@gmail.com>
2020-04-12 11:12:16 +0900

    Module 1

--- src/main/java/edu/hanyang/submit/TinySETokenizer.java
+++ src/main/java/edu/hanyang/submit/TinySETokenizer.java
@@ -1,19 +1,49 @@
 package edu.hanyang.submit;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.tartarus.snowball.ext.PorterStemmer;
 import edu.hanyang.indexer.Tokenizer;
 
 public class TinySETokenizer implements Tokenizer {
+   
+   SimpleAnalyzer analyzer;
+   PorterStemmer stemmer;
+   
+   public void setup() {
+      analyzer = new SimpleAnalyzer();
+      stemmer = new PorterStemmer();
+   }
 
-	public void setup() {
-	}
+   public List<String> split(String text) {
+      
+      List<String> result = new ArrayList<String>();
+      
+      try { 
+         TokenStream  stream = analyzer.tokenStream(null, new StringReader(text));
+         stream.reset();
+         CharTermAttribute term = stream.getAttribute(CharTermAttribute.class);
+         
+         while (stream.incrementToken()) {
+             stemmer.setCurrent(term.toString());
+            stemmer.stem();
+            System.out.println(stemmer.getCurrent());
+         }
+         stream.close();
+      } 
+      catch (IOException e) {
+         throw new RuntimeException(e);
+      }
+      return result;
+   } 
 
-	public List<String> split(String text) {
-		return null;
-	}
-
-	public void clean() {
-	}
+   public void clean() {
+      analyzer.close();
+   }
 
 }
